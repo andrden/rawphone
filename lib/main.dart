4c776d15f0352e8fb9f15d921567f877a902b229
andrii.denysenko <andrii.denysenko@4shared.com>
2021-04-04 13:44:05 +0300

    wave file header from scratch, not by taking an existing short WAV file

--- lib/main.dart
+++ lib/main.dart
@@ -305,6 +305,25 @@ sg8GK6gj9xA/Dl0D+vLKASUI6wDbBpsKdv5bCusL1P2t/8cQ1gjiBUgLhQfK/zgC+eWD4nXwBuZy
 //List<List<dynamic>> data = [];
 List<int> recordedData = [];
 
+Uint8List wavFileHeader(int soundDataLen) {
+  // http://soundfile.sapp.org/doc/WaveFormat/
+  var ret = Uint8List(4 * 11);
+  var bd = ret.buffer.asByteData();
+  bd.setUint32(0, 0x52494646, Endian.big); // RIFF
+  bd.setUint32(4, 36+soundDataLen, Endian.little);
+  bd.setUint32(8, 0x57415645, Endian.big); // WAVE
+  bd.setUint32(12, 0x666d7420, Endian.big); // 'fmt '
+  bd.setUint32(16, 16, Endian.little); // 16 bytes len of 'fmt ' subchunk
+  bd.setUint32(20, 0x01000100, Endian.big); // PCM = 1 MONO=1 channel
+  bd.setUint32(24, 8000, Endian.little); // sample rate
+  bd.setUint32(28, 2*8000, Endian.little); // byte rate
+  bd.setUint32(32, 0x02001000, Endian.big); // 2=BlockAlign       == NumChannels * BitsPerSample/8;  16 = BitsPerSample
+  bd.setUint32(36, 0x64617461, Endian.big); // 'data'
+  bd.setUint32(40, soundDataLen, Endian.little);
+
+  return ret;
+}
+
 class Src extends StreamAudioSource {
   Src(tag) : super(tag);
 
@@ -317,18 +336,34 @@ class Src extends StreamAudioSource {
 // Create simple PCM WAV:
 // ffmpeg -i NicoA1.webm -t 1 -ar 8000 -ac 1 a.wav
 
+    var byteData = data.buffer.asByteData();
     for (int i = 0; i < 8000; i++) {
-      data[data.length - 8000 * 2 + i * 2] = 0;
-      //data[data.length - 8000 * 2 + i * 2 + 1] = (50 + sin(i) * 50).floor();
-      data[data.length - 8000 * 2 + i * 2 + 1] =
-          (50 + recordedData[i] / 256).round();
+      byteData.setInt16(
+          data.length - 8000 * 2 + i * 2, recordedData[i], Endian.little);
+      // data[data.length - 8000 * 2 + i * 2] = 0;
+      // //data[data.length - 8000 * 2 + i * 2 + 1] = (50 + sin(i) * 50).floor();
+      // data[data.length - 8000 * 2 + i * 2 + 1] =
+      //     (50 + recordedData[i] / 256).round();
     }
 
+    var b = BytesBuilder();
+    var list2 = Uint8List(8000 * 2);
+    var byteData2 = list2.buffer.asByteData();
+    for (int i = 0; i < 8000; i++) {
+      byteData2.setInt16(
+          i * 2, (50 + sin(i) * 50).floor() * 256, Endian.little);
+      // data.add(0);
+      // data.add((50 + sin(i) * 50).floor());
+    }
+    b.add(data);
+    b.add(list2);
+
     return StreamAudioResponse(
-        sourceLength: data.length,
-        contentLength: data.length,
+        sourceLength: data.length + 8000 * 2,
+        contentLength: data.length + 8000 * 2,
         offset: 0,
-        stream: Stream.value(data),
+        //stream: Stream.value(data),
+        stream: Stream.value(b.toBytes()),
         contentType: "audio/wav");
   }
 }
